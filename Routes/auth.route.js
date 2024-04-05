212050a86545b8235ef1928f4eb7d2798a31cdeb
Dharmik Gohil <86297731+DharmikGohil@users.noreply.github.com>
2024-04-06 00:41:06 +0530

    Update auth.route.js

--- Routes/auth.route.js
+++ Routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { signup,login,logout } from "../Controllers/auth.controller.js";
+
 
 const router = express.Router();
 
